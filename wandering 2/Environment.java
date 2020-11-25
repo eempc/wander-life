0e32ae757f8e36a8386650b819bcec21c4031fe0
eempc <evc484@gmail.com>
2020-11-25 11:29:29 +0000

    cleaned up

--- wandering 2/Environment.java
+++ wandering 2/Environment.java
@@ -27,7 +27,6 @@ public class Environment {
 
     private Random rng;
 
-    //private ArrayList<Cell> tempNewLiveCells;
     /**
      * Create an environment with the default size.
      */
@@ -43,7 +42,6 @@ public class Environment {
      */
     public Environment(int numRows, int numCols) {
         rng = new Random();
-        //tempNewLiveCells = new ArrayList<>();
         nRows = numRows;
         nCols = numCols;
         setup(numRows, numCols);
@@ -51,14 +49,6 @@ public class Environment {
         view = new EnvironmentView(this, numRows, numCols);
         view.showCells();
         liveCells = new ArrayList<>();
-    }
-
-    public int getRows() {
-        return nRows;
-    }
-
-    public int getCols() {
-        return nCols;
     }
 
     /**
@@ -91,7 +81,7 @@ public class Environment {
             if (currentCellColumn != 0){
                 choices.add(cells[currentCellRow][currentCellColumn - 1]);
             }
-            
+
             // If not bottom row, add the south
             if (currentCellRow != numRows - 1) {
                 choices.add(cells[currentCellRow + 1][currentCellColumn]);
@@ -104,87 +94,6 @@ public class Environment {
 
             int cat = rng.nextInt(choices.size());
             tempNewLiveCells.add(choices.get(cat));
-
-            // Assign this cell's neighbours - edge detection required
-//            Cell north = (currentCellRow != 0) ? cells[currentCellRow + 1][currentCellColumn] : new Cell(-1, -1);
-//            Cell east = (currentCellColumn != 0) ? cells[currentCellRow][currentCellColumn + 1] : new Cell(-1, -1);
-//            Cell south = (currentCellRow != numRows - 1) ? cells[currentCellRow - 1][currentCellColumn] : new Cell(-1, -1);
-//            Cell west = (currentCellColumn != numCols - 1) ? cells[currentCellRow][currentCellColumn -1] : new Cell(-1, -1);
-
-            // Edge and corner detection
-            // 0 = north, 1 = east, 2 = south, 3 = west
-//            if (currentCellRow == 0 && currentCellColumn == 0) {
-//                // Go south or east 2 or 1
-//                Cell[] topLeftCornerChoices = { east, south };
-//                int x = rng.nextInt(topLeftCornerChoices.length);
-//                tempNewLiveCells.add(topLeftCornerChoices[x]);
-//            }
-//            else if (currentCellRow == 0 && currentCellColumn == numCols - 1) {
-//                // go south or west 2 or 3
-//                Cell[] topRightCornerChoices = { west, south };
-//                int x = rng.nextInt(topRightCornerChoices.length);
-//                tempNewLiveCells.add(topRightCornerChoices[x]);
-//            }
-//            else if (currentCellRow == numRows - 1 && currentCellColumn == 0) {
-//                // go north or east 0 or 1
-//                Cell[] bottomLeftChoices = { north, east };
-//                int x = rng.nextInt(bottomLeftChoices.length);
-//                tempNewLiveCells.add(bottomLeftChoices[x]);
-//            }
-//            else if (currentCellRow == numRows - 1 && currentCellColumn == numCols - 1) {
-//                // go north or west, 0 or 3
-//                Cell[] bottomRightChoices = { north, west };
-//                int x = rng.nextInt(bottomRightChoices.length);
-//                tempNewLiveCells.add(bottomRightChoices[x]);
-//            }
-//            else if (currentCellRow == 0) {
-//                // go go south, east or west, 1, 2, 3
-//                Cell[] topChoices = { south, east, west };
-//                int x = rng.nextInt(topChoices.length);
-//                tempNewLiveCells.add(topChoices[x]);
-//            }
-//            else if (currentCellRow == numRows - 1) {
-//                // go north, east or west, 0, 1, 3
-//                Cell[] bottomChoices = { north, east, west };
-//                int x = rng.nextInt(bottomChoices.length);
-//                tempNewLiveCells.add(bottomChoices[x]);
-//            }
-//            else if (currentCellColumn == 0) {
-//                // go east, north or south 0 1 2
-//                Cell[] leftChoices = { north, east, south };
-//                int x = rng.nextInt(leftChoices.length);
-//                tempNewLiveCells.add(leftChoices[x]);
-//            }
-//            else if (currentCellColumn == numCols - 1) {
-//                // go west, north or south, 0 , 2, 3
-//                Cell[] rightChoices = { north, west, south };
-//                int x = rng.nextInt(rightChoices.length);
-//                tempNewLiveCells.add(rightChoices[x]);
-//            }
-//            else {
-//                Cell[] allChoices = {north, west, south, east};
-//                int x = rng.nextInt(allChoices.length);
-//                tempNewLiveCells.add(allChoices[x]);
-//            }
-
-
-            // Basic logic for moving cells without taking into account the edges/corners
-//            int dir = rng.nextInt(4);
-//
-//            switch (dir) {
-//                case 0:
-//                    tempNewLiveCells.add(north); // north
-//                    break;
-//                case 1:
-//                    tempNewLiveCells.add(east); // east
-//                    break;
-//                case 2:
-//                    tempNewLiveCells.add(south); // south
-//                    break;
-//                case 3:
-//                    tempNewLiveCells.add(west); // west
-//                    break;
-//            }
 
             // Kill current live cell
             setCellState(currentCellRow, currentCellColumn, Cell.DEAD);
